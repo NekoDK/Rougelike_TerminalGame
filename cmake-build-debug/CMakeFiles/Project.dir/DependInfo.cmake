e338ca35caa3090089d8787364011495eef3d5df
NekoDK <65958329+NekoDK@users.noreply.github.com>
2023-09-30 13:36:50 +0600

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/Project.dir/DependInfo.cmake
@@ -0,0 +1,45 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/bulba_daddy/kolomiets.dmitriy-game/src/Coins.cpp" "CMakeFiles/Project.dir/src/Coins.cpp.o" "gcc" "CMakeFiles/Project.dir/src/Coins.cpp.o.d"
+  "/home/bulba_daddy/kolomiets.dmitriy-game/src/Controls.cpp" "CMakeFiles/Project.dir/src/Controls.cpp.o" "gcc" "CMakeFiles/Project.dir/src/Controls.cpp.o.d"
+  "/home/bulba_daddy/kolomiets.dmitriy-game/src/Food.cpp" "CMakeFiles/Project.dir/src/Food.cpp.o" "gcc" "CMakeFiles/Project.dir/src/Food.cpp.o.d"
+  "/home/bulba_daddy/kolomiets.dmitriy-game/src/Math-utils.cpp" "CMakeFiles/Project.dir/src/Math-utils.cpp.o" "gcc" "CMakeFiles/Project.dir/src/Math-utils.cpp.o.d"
+  "/home/bulba_daddy/kolomiets.dmitriy-game/src/Obstacle-manager.cpp" "CMakeFiles/Project.dir/src/Obstacle-manager.cpp.o" "gcc" "CMakeFiles/Project.dir/src/Obstacle-manager.cpp.o.d"
+  "/home/bulba_daddy/kolomiets.dmitriy-game/src/Obstacle.cpp" "CMakeFiles/Project.dir/src/Obstacle.cpp.o" "gcc" "CMakeFiles/Project.dir/src/Obstacle.cpp.o.d"
+  "/home/bulba_daddy/kolomiets.dmitriy-game/src/Player.cpp" "CMakeFiles/Project.dir/src/Player.cpp.o" "gcc" "CMakeFiles/Project.dir/src/Player.cpp.o.d"
+  "/home/bulba_daddy/kolomiets.dmitriy-game/src/TextLevelReader.cpp" "CMakeFiles/Project.dir/src/TextLevelReader.cpp.o" "gcc" "CMakeFiles/Project.dir/src/TextLevelReader.cpp.o.d"
+  "/home/bulba_daddy/kolomiets.dmitriy-game/src/lib/scene_manager.cpp" "CMakeFiles/Project.dir/src/lib/scene_manager.cpp.o" "gcc" "CMakeFiles/Project.dir/src/lib/scene_manager.cpp.o.d"
+  "/home/bulba_daddy/kolomiets.dmitriy-game/src/proj/Project.cpp" "CMakeFiles/Project.dir/src/proj/Project.cpp.o" "gcc" "CMakeFiles/Project.dir/src/proj/Project.cpp.o.d"
+  "/home/bulba_daddy/kolomiets.dmitriy-game/src/proj/math-utils.cpp" "CMakeFiles/Project.dir/src/proj/math-utils.cpp.o" "gcc" "CMakeFiles/Project.dir/src/proj/math-utils.cpp.o.d"
+  "/home/bulba_daddy/kolomiets.dmitriy-game/src/proj/scenes/game_over_scene.cpp" "CMakeFiles/Project.dir/src/proj/scenes/game_over_scene.cpp.o" "gcc" "CMakeFiles/Project.dir/src/proj/scenes/game_over_scene.cpp.o.d"
+  "/home/bulba_daddy/kolomiets.dmitriy-game/src/proj/scenes/game_scene.cpp" "CMakeFiles/Project.dir/src/proj/scenes/game_scene.cpp.o" "gcc" "CMakeFiles/Project.dir/src/proj/scenes/game_scene.cpp.o.d"
+  "/home/bulba_daddy/kolomiets.dmitriy-game/src/proj/scenes/game_scene2.cpp" "CMakeFiles/Project.dir/src/proj/scenes/game_scene2.cpp.o" "gcc" "CMakeFiles/Project.dir/src/proj/scenes/game_scene2.cpp.o.d"
+  "/home/bulba_daddy/kolomiets.dmitriy-game/src/proj/scenes/title_scene.cpp" "CMakeFiles/Project.dir/src/proj/scenes/title_scene.cpp.o" "gcc" "CMakeFiles/Project.dir/src/proj/scenes/title_scene.cpp.o.d"
+  "/home/bulba_daddy/kolomiets.dmitriy-game/src/proj/systems/ai_system.cpp" "CMakeFiles/Project.dir/src/proj/systems/ai_system.cpp.o" "gcc" "CMakeFiles/Project.dir/src/proj/systems/ai_system.cpp.o.d"
+  "/home/bulba_daddy/kolomiets.dmitriy-game/src/proj/systems/collision_system.cpp" "CMakeFiles/Project.dir/src/proj/systems/collision_system.cpp.o" "gcc" "CMakeFiles/Project.dir/src/proj/systems/collision_system.cpp.o.d"
+  "/home/bulba_daddy/kolomiets.dmitriy-game/src/proj/systems/delete_sysytem.cpp" "CMakeFiles/Project.dir/src/proj/systems/delete_sysytem.cpp.o" "gcc" "CMakeFiles/Project.dir/src/proj/systems/delete_sysytem.cpp.o.d"
+  "/home/bulba_daddy/kolomiets.dmitriy-game/src/proj/systems/game_over_system.cpp" "CMakeFiles/Project.dir/src/proj/systems/game_over_system.cpp.o" "gcc" "CMakeFiles/Project.dir/src/proj/systems/game_over_system.cpp.o.d"
+  "/home/bulba_daddy/kolomiets.dmitriy-game/src/proj/systems/game_win_system.cpp" "CMakeFiles/Project.dir/src/proj/systems/game_win_system.cpp.o" "gcc" "CMakeFiles/Project.dir/src/proj/systems/game_win_system.cpp.o.d"
+  "/home/bulba_daddy/kolomiets.dmitriy-game/src/proj/systems/gravity_system.cpp" "CMakeFiles/Project.dir/src/proj/systems/gravity_system.cpp.o" "gcc" "CMakeFiles/Project.dir/src/proj/systems/gravity_system.cpp.o.d"
+  "/home/bulba_daddy/kolomiets.dmitriy-game/src/proj/systems/jump_control_system.cpp" "CMakeFiles/Project.dir/src/proj/systems/jump_control_system.cpp.o" "gcc" "CMakeFiles/Project.dir/src/proj/systems/jump_control_system.cpp.o.d"
+  "/home/bulba_daddy/kolomiets.dmitriy-game/src/proj/systems/movement_system.cpp" "CMakeFiles/Project.dir/src/proj/systems/movement_system.cpp.o" "gcc" "CMakeFiles/Project.dir/src/proj/systems/movement_system.cpp.o.d"
+  "/home/bulba_daddy/kolomiets.dmitriy-game/src/proj/systems/obstacles_control_system.cpp" "CMakeFiles/Project.dir/src/proj/systems/obstacles_control_system.cpp.o" "gcc" "CMakeFiles/Project.dir/src/proj/systems/obstacles_control_system.cpp.o.d"
+  "/home/bulba_daddy/kolomiets.dmitriy-game/src/proj/systems/precollision_system.cpp" "CMakeFiles/Project.dir/src/proj/systems/precollision_system.cpp.o" "gcc" "CMakeFiles/Project.dir/src/proj/systems/precollision_system.cpp.o.d"
+  "/home/bulba_daddy/kolomiets.dmitriy-game/src/proj/systems/rendering_system.cpp" "CMakeFiles/Project.dir/src/proj/systems/rendering_system.cpp.o" "gcc" "CMakeFiles/Project.dir/src/proj/systems/rendering_system.cpp.o.d"
+  "/home/bulba_daddy/kolomiets.dmitriy-game/src/proj/systems/statistic_system.cpp" "CMakeFiles/Project.dir/src/proj/systems/statistic_system.cpp.o" "gcc" "CMakeFiles/Project.dir/src/proj/systems/statistic_system.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
